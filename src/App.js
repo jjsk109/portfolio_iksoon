24dd764ff790afc8b70b428efe04102c7dd48f0f
jjsk109 <jjsk109@gmail.com>
2021-08-16 22:49:02 +0900

    google map add study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import Calendar from 'react-calendar';
 import './App.css';
 import SelfIntroduction from './selfIntroduction/selfIntroduction';
 import 'react-calendar/dist/Calendar.css';
+import GoogleMapReact  from 'google-map-react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <ul>
-            <li>자기소개</li>
-            <li>관심사</li>
-          </ul>
-        </header>
-        <SelfIntroduction/>
-        <Calendar/>
-      </div>
-    );
-  }
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+function App() {
+  const [value, onChange] = useState(new Date());
+  const defaultProps = {
+    center: {
+      lat: 37.0083705,
+      lng: 127.0802772
+    },
+    zoom: 11
+  };
+  console.log(defaultProps);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>자기소개</li>
+          <li>관심사</li>
+        </ul>
+      </header>
+      <SelfIntroduction/>
+      <Calendar
+        onChange={onChange}
+        value={value}  
+      />
+       <div style={{ height: '50vh', width: '50%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+    </div>
+  );
+  
 }
 
 export default App;
