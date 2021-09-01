6f861f1c0baed88b2ac334e4d4f4b66561a26004
jjsk109 <jjsk109@gmail.com>
2021-09-02 01:30:33 +0900

    canvas draw update

--- src/my_canvas/my_canvas.jsx
+++ src/my_canvas/my_canvas.jsx
@@ -6,50 +6,50 @@ import { useEffect } from 'react';
 
 
 const Mycanvas = (props) => {
-    
     const canvasRef = useRef(null);
     const [ctx, setCtx] = useState([]);
-    const [canvasTag, setCanvasTag] = useState([]);
-
-    const color = useRef('white');
+    const [drawColor,setColor] = useState('white');
+    const [drawColorClass,setColorClass] = useState(styles.white);
     const [draw, setDraw] = useState(false);
+    const [size, setSize] = useState(20);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        console.log( canvas);
         const context = canvas.getContext("2d");
-        // context.strokeStyle = "white";
         
         setCtx(context);
     }, []);
 
     const drawStart = () => {
-        console.log("drawStart");
         setDraw(true);
     }
     const drawEnd = () => {
-        console.log("drawEnd");
         setDraw(false);
     }
     const finishDrawing = () => {
-        console.log("drawEnd");
         setDraw(false);
     }
     const drawHandel = event => {
         if(draw){
-            console.log("drawHandel");
             ctx.beginPath();
-            ctx.arc(event.clientX ,event.clientY,2.5,Math.PI * 2, false);
-            ctx.arc(900 ,500,0,Math.PI * 2, true);
-            ctx.fillStyle='red';
+            ctx.arc(event.clientX ,event.clientY,size,Math.PI * 2, false);
+            ctx.fillStyle=`${drawColor}`;
             ctx.fill();
-            console.log(event.clientX ,event.clientY );
-           
         }
     }
-
+    const changeColor = selectColor =>{
+        console.log(selectColor.target.value);
+        const color = selectColor.target.value;
+        setColor(color);
+        setColorClass(selectColor.target.className)
+    }
+    const changeSize = event => {
+        const size = event.target.value > 100 ? 100 : event.target.value;
+        const resize =   size < 0 ? 1 :  size;
+        setSize(resize);
+    }
     return(
             <div>
                 <canvas className={styles.can} ref={canvasRef} 
@@ -58,15 +58,20 @@ const Mycanvas = (props) => {
                     onMouseMove={drawHandel}
                     onMouseLeave={finishDrawing}
                 ></canvas>
-                <div className={styles.button_box}>
-                    <button className={ styles.red}></button>
-                    <button className={ styles.blue}></button>
-                    <button className={ styles.yello}></button>
-                    <button className={ styles.white}></button>
-                    <button className={ styles.brown}></button>
-                    <button className={ styles.green}></button>
-                    <button className={ styles.pink}></button>
+                <div className={styles.button_box}> 
+                    <button className={styles.white} onClick={changeColor} value="white"></button>
+                    <button className={styles.black} onClick={changeColor} value="black"></button>
+                    <button className={styles.red} onClick={changeColor} value="red" ></button>
+                    <button className={styles.blue} onClick={changeColor} value="blue"></button>
+                    <button className={styles.yellow} onClick={changeColor} value="yellow"></button>
+                    <button className={styles.brown} onClick={changeColor} value="brown"></button>
+                    <button className={styles.green} onClick={changeColor} value="green"></button>
+                    <button className={styles.pink} onClick={changeColor} value="pink"></button>
+                    <input type="range" id="vol" name="vol"  value={size} onChange={changeSize} min="0" max="100"></input>
+                    <input type="number" min="0" max="100" onChange={changeSize} value={size}/>
+                    <div className={drawColorClass}></div>
                 </div>
+                
             </div>
     )
 };
