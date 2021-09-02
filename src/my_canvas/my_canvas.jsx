13561742b889864a11155d20a0422f7bc9988448
jjsk109 <jjsk109@gmail.com>
2021-09-02 13:35:54 +0900

    i don't know how make ctrl z

--- src/my_canvas/my_canvas.jsx
+++ src/my_canvas/my_canvas.jsx
@@ -12,18 +12,30 @@ const Mycanvas = (props) => {
     const [drawColorClass,setColorClass] = useState(styles.white);
     const [draw, setDraw] = useState(false);
     const [size, setSize] = useState(20);
+    const [canvasWidth, setCanvasWidth] = useState(0);
+    const [canvaHeight, setCanvasHeight] = useState(0);
+    const [commends,setComends] = useState([]);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        const context = canvas.getContext("2d");
-        
+        const context = canvas.getContext("2d"); 
         setCtx(context);
+        setCanvasWidth( canvas.width);
+        setCanvasHeight(canvas.height);
     }, []);
 
-    const drawStart = () => {
+    const drawStart = event => {
         setDraw(true);
+        ctx.beginPath();
+        ctx.arc(event.clientX ,event.clientY,size,Math.PI * 2, false);
+        ctx.fillStyle=`${drawColor}`;
+        ctx.fill();
+        const setCon = {x:event.clientX ,y:event.clientY,size:size,color:drawColor};
+        const newcon = [setCon,...commends];
+        setComends(newcon);
+        console.log(commends);
     }
     const drawEnd = () => {
         setDraw(false);
@@ -37,10 +49,12 @@ const Mycanvas = (props) => {
             ctx.arc(event.clientX ,event.clientY,size,Math.PI * 2, false);
             ctx.fillStyle=`${drawColor}`;
             ctx.fill();
+            const setCon = {x:event.clientX ,y:event.clientY,size:size,color:drawColor};
+            const newcon = [setCon,...commends];
+            setComends(newcon);
         }
     }
     const changeColor = selectColor =>{
-        console.log(selectColor.target.value);
         const color = selectColor.target.value;
         setColor(color);
         setColorClass(selectColor.target.className)
@@ -50,6 +64,23 @@ const Mycanvas = (props) => {
         const resize =   size < 0 ? 1 :  size;
         setSize(resize);
     }
+    const resetHandel = () => {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, canvasWidth, canvaHeight);
+    }
+   
+    const saveHandel = () => {
+        console.log("save");
+    }
+    const handleKeys = event => {
+        if(event.ctrlKey && event.key ==='z'){
+            const con = commends.shift();
+            console.log(commends);
+           // console.log(con);
+        }
+    }
+    window.addEventListener('keydown', handleKeys);
+
     return(
             <div>
                 <canvas className={styles.can} ref={canvasRef} 
@@ -57,7 +88,11 @@ const Mycanvas = (props) => {
                     onMouseUp={drawEnd}
                     onMouseMove={drawHandel}
                     onMouseLeave={finishDrawing}
+                    
+                   
                 ></canvas>
+                <button className={styles.reset} onClick={resetHandel}>reset</button>
+                <button className={styles.reset} onClick={saveHandel}>save</button>
                 <div className={styles.button_box}> 
                     <button className={styles.white} onClick={changeColor} value="white"></button>
                     <button className={styles.black} onClick={changeColor} value="black"></button>
@@ -71,7 +106,6 @@ const Mycanvas = (props) => {
                     <input type="number" min="0" max="100" onChange={changeSize} value={size}/>
                     <div className={drawColorClass}></div>
                 </div>
-                
             </div>
     )
 };
