f34df85411b08f1d26990c9a537f2c1e64c5ec14
jjsk109 <jjsk109@gmail.com>
2021-09-09 14:46:04 +0900

    server start

--- src/header/portfolioHeader.jsx
+++ src/header/portfolioHeader.jsx
@@ -1,6 +1,6 @@
 import {React} from 'react';
 import styles from './portfolioHeader.module.css';
-import { faAddressBook, faAddressCard, faBox, faCamera, faImage, faImages } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook, faAddressCard, faBox, faCamera, faCaravan, faImage, faImages } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const PortfolioHeader = ({setPage}) => {
@@ -25,7 +25,7 @@ const PortfolioHeader = ({setPage}) => {
             <li className={styles.tag}><a onClick={onClickSelf} ><FontAwesomeIcon icon={faAddressCard} size="1x" />  자기소개</a></li>
             <li className={styles.tag}><a onClick={onClickAsw} > <FontAwesomeIcon icon={faBox} size="1x" />이것저것</a></li>
             <li className={styles.tag}><a onClick={onClickHappit}> <FontAwesomeIcon icon={faImages} size="1x" />겔러리</a></li>
-            <li className={styles.tag}><a onClick={onClickCanvas}> <FontAwesomeIcon icon={faImages} size="1x" />켄버스</a></li>
+            <li className={styles.tag}><a onClick={onClickCanvas}> <FontAwesomeIcon icon={faCaravan} size="1x" />켄버스</a></li>
         </ul>
     </header>
 )};
